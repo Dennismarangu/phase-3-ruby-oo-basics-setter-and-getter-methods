7ad20d6545ea33d34ee891914e6145e79c4e65e6
Dennismarangu <dennismutumamarangu@gmail.com>
2023-05-21 09:53:54 +0300

    Not yet completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,12 @@
-class Dog
+require 'pry'
 
+class Dog
+  attr_accessor :name, :breed
+  
+  def initialize(name, breed)
+    @name = name
+    @breed = breed
+  end
 end
+
+dog = Dog.new("Buddy", "Labrador Retriever")
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,13 @@
-class Person
+ require 'pry'
 
-end
+class Person
+    attr_accessor :name, :job
+    
+    def initialize(name, job)
+      @name = name
+      @job = job
+    end
+  end
+  
+  person = Person.new("John", "Developer")
+
